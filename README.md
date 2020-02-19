22c148b6b1cdddcfd5900f6b39180d491c78b22b
Adam Zammit <adam.zammit@acspri.org.au>
2020-02-19 16:28:35 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A Python 3 script for reading data from LimeSurvey in a format that Power BI will understand
+
+1. Install Python
+2. pip install pandas
+3. pip install git+https://github.com/ESchae/limesurveyrc2api
+4. Update the script to include your LimeSurvey JSON-RPC URL, username, password and survey id (Hint: make a new copy of this script for each survey)
+5. Run the script from the Power BI desktop
---
+++ limesurvey-powerbi.py
@@ -0,0 +1,42 @@
+import pandas as pd #For data frames in a format PowerBI understands
+from limesurveyrc2api.limesurvey import LimeSurvey #To talk to LimeSurvey API
+from limesurveyrc2api.exceptions import LimeSurveyError
+import base64 #To convert base64 encoded data
+import io #To convert a string to a buffer for pandas to read
+
+#Limesurvey details - change these to match your Survey
+remotecontrolurl = "http://localhost/limesurvey/index.php/admin/remotecontrol"
+limeuser = "admin"
+limepasswd = "password"
+surveyid = "1"  
+
+#Code to call the LimeSurvey API to extract data
+ 
+#Connect to LimeSurvey
+lime = LimeSurvey(url=remotecontrolurl,username=limeuser)
+lime.open(password=limepasswd)
+#Read using the survey export responses API call
+result = lime.survey.export_responses(survey_id=surveyid,document_type='csv',heading_type='full',response_type='long')
+#Convert data to a pandas data frame
+surveydata = pd.read_csv(io.StringIO(base64.b64decode(result).decode("utf-8")),sep=';')
+#print(surveydata)
+#Read using the token list participants API call
+attlist = []
+for x in range(1,255):
+    attlist.append("attribute_" + str(x))
+result = lime.token.list_participants(survey_id=surveyid,start=0,limit=100000,attributes=attlist)
+#Flatten out participant_info and add attributes
+nl = []
+for x in result:
+    items = {'tid': x['tid'], 'token': x['token'], 'firstname': x['participant_info']['firstname'], 'lastname': x['participant_info']['lastname'], 'email': x['participant_info']['email']}
+    for att in attlist:
+        if att in x:
+            items[att] = x[att]
+    nl.append(items)
+#Convert data to a pandas data frame
+tokendata = pd.DataFrame(nl)
+#print(tokendata)
+mergeddata = tokendata.merge(right=surveydata,how='outer',left_on='token',right_on='Token')
+#print(mergeddata)
+#Disconnect
+lime.close()
